de15b3a6ca69abe961a3be7bc4804dd37177f0d5
Mihail Lytov <misha.lytov@yadex.ru>
2025-12-18 00:03:42 +0300

    fix: core

--- src/main/java/com/lytov/diplom/dsppranalyzer/DSpprAnalyzerApplication.java
+++ src/main/java/com/lytov/diplom/dsppranalyzer/DSpprAnalyzerApplication.java
@@ -1,9 +1,13 @@
 package com.lytov.diplom.dsppranalyzer;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
 public class DSpprAnalyzerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DSpprAnalyzerApplication.class, args);
+    }
 }
